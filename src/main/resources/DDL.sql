eb305430da91a709410c770134563826e659ac60
jovchinnikova <juliya.ovcha@gmail.com>
2021-12-09 11:51:13 +0300

    DDL was created

---
+++ src/main/resources/DDL.sql
@@ -0,0 +1,39 @@
+create database atm;
+use atm;
+
+create table if not exists Banks (
+id serial,
+name varchar(50) not null unique,
+primary key (id)
+);
+
+create table if not exists ATMs (
+id serial,
+cash int unsigned,
+bank_id bigint unsigned not null,
+primary key (id),
+constraint fk_ATMs_Bank foreign key(bank_id) references Banks (id)
+on update no action on delete cascade
+);
+
+create table if not exists Accounts (
+id serial,
+money decimal(20,2),
+account_number int unsigned,
+lock_status boolean not null,
+bank_id bigint unsigned not null,
+primary key (id),
+constraint fk_Accounts_Bank foreign key (bank_id) references Banks (id)
+on update no action on delete cascade
+);
+
+create table if not exists Cards (
+id serial,
+card_number int unsigned not null,
+pin int unsigned not null,
+account_id bigint unsigned not null,
+primary key (id),
+constraint fk_Cards_Account foreign key (account_id) references Accounts (id)
+on update no action on delete cascade
+);
+
