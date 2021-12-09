cd561fab2408c9865af035bb6f85c95efec7c6b5
Scratch-z <77450234+Scratch-z@users.noreply.github.com>
2021-12-09 14:25:41 +0300

    Main was added

--- src/main/java/com/solvd/atm/service/impl/AccountServiceImpl.java
+++ src/main/java/com/solvd/atm/service/impl/AccountServiceImpl.java
@@ -1,26 +1,26 @@
-package com.solvd.atm.service.impl;
-
-import com.solvd.atm.domain.Account;
-import com.solvd.atm.domain.Card;
-import com.solvd.atm.service.AccountService;
-
-public class AccountServiceImpl implements AccountService {
-
-    private final AccountRepository accountRepository;
-    private final CardService cardService;
-
-    public AccountServiceImpl() {
-        this.accountRepository = new AccountRepositoryImpl();
-        this.cardService = new CardServiceImpl();
-    }
-
-    @Override
-    public Account getAccountInfo(Card cardInfo) {
-
-    }
-
-    @Override
-    public void decrementMoney(Account account, Integer money) {
-
-    }
-}
+//package com.solvd.atm.service.impl;
+//
+//import com.solvd.atm.domain.Account;
+//import com.solvd.atm.domain.Card;
+//import com.solvd.atm.service.AccountService;
+//
+//public class AccountServiceImpl implements AccountService {
+//
+//    private final AccountRepository accountRepository;
+//    private final CardService cardService;
+//
+//    public AccountServiceImpl() {
+//        this.accountRepository = new AccountRepositoryImpl();
+//        this.cardService = new CardServiceImpl();
+//    }
+//
+//    @Override
+//    public Account getAccountInfo(Card cardInfo) {
+//
+//    }
+//
+//    @Override
+//    public void decrementMoney(Account account, Integer money) {
+//
+//    }
+//}
